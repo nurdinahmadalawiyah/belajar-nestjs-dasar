4a65a2de8dae70442343e53492eeebc988fc33ad
Nurdin Alawiyah <nurdinalawiyah@192.168.1.5>
2024-04-03 14:12:13 +0700

    lifecyle event

--- src/prisma/prisma/prisma.service.ts
+++ src/prisma/prisma/prisma.service.ts
@@ -1,10 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import * as console from 'console';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   constructor() {
     super();
     console.info('Create Prisma Service');
+  }
+
+  onModuleInit() {
+    console.info('Connect Prisma');
+    this.$connect();
+  }
+
+  onModuleDestroy() {
+    console.info('Disconnect Prisma');
+    this.$disconnect();
   }
 }
